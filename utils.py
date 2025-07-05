1914e281b6977d5a56819262d3fa45a532805ee2
Pedro_Csta <pedropfc.costa@gmail.com>
2025-07-04 21:35:28 -0300

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,8 @@
+# utils.py
+
+# Nomes das Personas para cada produto (agora, área de estudo)
+PERSONA_NAMES = {
+    "Product Management": "Mariana, a Futura Product Manager",
+    "UX Design": "Lucas, o Designer em Transição",
+    "Data Analytics": "Beatriz, a Analista de Dados"
+}
